f361a4f77e3de314cc59a330d50c805fe678bbaf
Jacob Westman <jacob.devillusion@gmail.com>
2017-03-04 21:14:00 +0100

    Adding routes

---
+++ app/routes.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jacob on 2017-03-04.
+ */
+import React from 'react';
+import { Route, Redirect, IndexRoute} from 'react-router';
+import c from './components';
+
+const routes  =
+    <Route path="/" component={c.Layout}>
+        <Redirect from="/" to="/ranks" />
+        <IndexRoute component={c.FilterableRankingTable} />
+        <Route path="ranks" component={c.FilterableRankingTable}></Route>
+        <Route path="scores" component={c.Scoring}></Route>
+    </Route>
+
+export default routes;
